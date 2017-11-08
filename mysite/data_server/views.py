8ba65220c417750c3f08effad814ae5e4626ace7
HardPlant <abc7988se@naver.com>
2017-11-08 22:09:21 +0900

    update

--- mysite/data_server/views.py
+++ mysite/data_server/views.py
@@ -14,10 +14,30 @@ json_error_response = JsonResponse({'status': '500'})
 def handle_sound_data_list(request):
     print(request.body)
     if request.method == 'POST':
-        response = parse_data_packages(request)
-        return response
+        return parse_data_packages(request)
+    if request.method == 'GET':
+        return get_data_packages(request)
 
     return json_error_response
+
+
+def get_data_packages(request):
+    if request.GET.get('to_date') is None \
+        or request.GET.get('from-date') is None:
+        return json_error_response
+
+    to_date = request.GET.get('to_date')
+    from_date = request.GET.get('from-date')
+    res = []
+    data_list = SoundData.objects.filter(time__range=[from_date, to_date])
+
+    for item in data_list:
+        cur_dB = item['dB']
+        cur_time = str(item['time'])
+        res.append(json.dumps({'dB' : cur_dB,
+                               'time' : cur_time}))
+
+    return JsonResponse(json.dumps(res))
 
 
 def parse_data_packages(request):
@@ -29,6 +49,7 @@ def parse_data_packages(request):
         save_sound_data(sound_data)
 
     return JsonResponse({'status': '200'})
+
 
 def save_sound_data(received_data):
     req_date = datetime.datetime.strptime(received_data['time'],"%Y-%m-%d %H:%M:%S.%f")
@@ -49,11 +70,28 @@ def save_sound_data(received_data):
 
     data.save()
 
+def verify_sound_data(from_pk, to_pk):
+    assert(from_pk <= to_pk)
+
+    from_data = SoundData.objects.get(pk=from_pk)
+    to_data = SoundData.objects.get(pk=to_pk)
+    hash = from_data.hash
+
+    for i in range(from_pk+1,to_pk+1):
+        sha_256 = hashlib.sha256()
+        current_dB = SoundData.objects.get(i).dB
+        current_time = SoundData.objects.get(i).time
+        hash = sha_256.update(bytes(str(current_time)+ str(current_dB),'UTF-8')
+                              + hash)
+
+    if hash == to_data.hash:
+        return True
+    else:
+        return False
 
 def send_sound_data_list(data):
     url = 'Firebase'
     requests.post(url=url, data=data)
-
 
 def send_push():
     url = 'Firebase'
